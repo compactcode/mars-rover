b4f4fd43e97945af2b60fa5e395a05225f247479
Shanon McQuay <shanonmcquay@gmail.com>
2011-05-20 23:40:48 +1000

    Add the ability for rovers to execute commands.

--- lib/rover.rb
+++ lib/rover.rb
@@ -16,18 +16,32 @@ class Rover
     rotate(Direction::Rotation::Right)
   end
   
-  def rotate(rotation)
-    @direction = Direction.rotate(@direction, rotation)
-    self    
-  end
-  
   def move
     target_coordinates = translate(@coordinates, Direction.to_coordinates(direction))
     @coordinates = target_coordinates if @plateau.contains_coordinates(target_coordinates)
     self
   end
   
+  def execute_commands(commands)
+    commands.chars.each do |command|
+      case command
+      when "L"
+        rotate_left
+      when "R"
+        rotate_right
+      when "M"
+        move
+      end
+    end
+    self
+  end
+  
   protected
+  
+    def rotate(rotation)
+      @direction = Direction.rotate(@direction, rotation)
+      self    
+    end
   
     def translate(first, second)
       first.zip(second).map { |axis| axis.reduce(:+) }
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -4,27 +4,43 @@ describe Rover do
   
   describe "#rotate_left" do
     it "should change the current direction of the rover" do
-      Rover.new(Plateau.new([5, 5]), [1, 1], Direction::North).rotate_left.direction.should == Direction::West
+      create_rover.rotate_left.direction.should == Direction::West
     end
   end
   
   describe "#rotate_right" do
     it "should change the current direction of the rover" do
-      Rover.new(Plateau.new([5, 5]), [1, 1], Direction::North).rotate_right.direction.should == Direction::East
+      create_rover.rotate_right.direction.should == Direction::East
     end
   end
   
   describe "#move" do
     it "should move the rover in the current direction" do
-      Rover.new(Plateau.new([5, 5]), [1, 1], Direction::North).move.coordinates.should == [1, 2]
+      create_rover.move.coordinates.should == [1, 2]
     end
     it "should not move the rover if it is at the edge of the plateau" do
-      Rover.new(Plateau.new([5, 5]), [5, 5], Direction::North).move.coordinates.should == [5, 5]
+      create_rover([5, 5]).move.coordinates.should == [5, 5]
+    end
+  end
+  
+  describe "#execute_commands" do
+    it "should translate the 'L' command into a #rotate_left" do
+      create_rover.execute_commands("L").direction.should == Direction::West
+    end
+    it "should translate the 'R' command into a #rotate_right" do
+      create_rover.execute_commands("R").direction.should == Direction::East
+    end
+    it "should translate the 'M' command into a #move" do
+      create_rover.execute_commands("M").coordinates.should == [1, 2]
     end
   end
   
   it "should allow multiple commands to be issued" do
-    Rover.new(Plateau.new([5, 5]), [1, 1], Direction::North).rotate_left.rotate_right.move.coordinates.should == [1, 2]
+    create_rover.rotate_left.rotate_right.move.coordinates.should == [1, 2]
+  end
+  
+  def create_rover(start_coordinates = [1, 1])
+    Rover.new(Plateau.new([5, 5]), start_coordinates, Direction::North)
   end
   
 end
